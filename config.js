ab0d75f057f15209c13b0c4d57372e5e016639dc
justin <jtr342@gmail.com>
2021-01-04 22:10:03 -0800

    using openweather api

---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+  OPEN_WEATHER : 'be5ff8d787513cfca0d1d6829bdac737'
+}
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require ('express');
+const express = require ("express");
+const bodyParser = require("body-parser");
+const https = require("https");
 const app = express();
-
+const url = 
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', (req, res) => {
+    res.send("result: " + (Number(req.body.num1) + Number(req.body.num2)));
 })
 
 app.listen(3000, () => {
